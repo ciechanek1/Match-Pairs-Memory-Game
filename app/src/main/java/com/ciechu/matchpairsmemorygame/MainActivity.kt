2963b6914d7d6868d715e372e2dd6814bbcf8161
ciechanek1 <60982192+ciechanek1@users.noreply.github.com>
2020-07-15 11:01:18 +0200

    Init commit

---
+++ app/src/main/java/com/ciechu/matchpairsmemorygame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ciechu.matchpairsmemorygame
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
